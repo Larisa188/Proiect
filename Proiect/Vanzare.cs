d0dba803fac5e797aea6fe42e2f750e1e09b5f24
Larisa188 <larisa.carp@student.usv.ro>
2026-03-05 16:28:46 +0200

    Add files via upload

---
+++ Proiect/Vanzare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proiect
+{
+    public class Vanzare
+    {
+        public DateTime DataVanzarii { get; set; }
+        public Produs ProdusVandut { get; set; }
+        public int CantitateVanduta { get; set; }
+        public decimal TotalIncasat { get; set; }
+
+        public Vanzare(Produs produs, int cantitate)
+        {
+            DataVanzarii = DateTime.Now;
+            ProdusVandut = produs;
+            CantitateVanduta = cantitate;
+            TotalIncasat = cantitate * produs.PretPerUnitate;
+            produs.CantitateStoc = produs.CantitateStoc - cantitate;
+        }
+    }
+}
