765b532dfa375a9427c05a112f6f4bb5d35e672c
Larisa188 <larisa.carp@student.usv.ro>
2026-03-05 16:06:23 +0200

    adaugare clase

---
+++ Proiect/Produs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proiect
+{
+    public abstract class Produs
+    {
+        public string Denumire { get; set; }
+        public decimal PretPerUnitate { get; set; }
+        public int CantitateStoc { get; set; }
+        public string Producator { get; set; }
+    }
+}
